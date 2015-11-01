7cc6a12a1bdc469b77db20c446ce34b77dc9bc38
Tibor Toth <toth.t11@gmail.com>
2015-11-01 18:08:44 +0100

    add middleware

---
+++ EscherOwinMiddleware/EscherMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using EscherAuth.Middlewares.Owin.Request;
+using Microsoft.Owin;
+
+namespace EscherAuth.Middlewares.Owin
+{
+    public class EscherMiddleware
+    {
+        private readonly Func<IDictionary<string, object>, Task> next;
+        private readonly IKeyDb keyDb;
+        private readonly Escher escher = new Escher();
+        private readonly Action<string> logger;
+
+        public EscherMiddleware(Func<IDictionary<string, object>, Task> next, IKeyDb keyDb) : this(next, keyDb, null, null) { }
+
+        public EscherMiddleware(Func<IDictionary<string, object>, Task> next, IKeyDb keyDb, EscherConfig escherConfig) : this(next, keyDb, escherConfig, null) { }
+
+        public EscherMiddleware(Func<IDictionary<string, object>, Task> next, IKeyDb keyDb, EscherConfig escherConfig, Action<string> logger)
+        {
+            this.next = next;
+            this.keyDb = keyDb;
+            this.escher.Config = escherConfig ?? this.escher.Config;
+            this.logger = logger ?? (message => Debug.WriteLine(message));
+        }
+
+        public async Task Invoke(IDictionary<string, object> env)
+        {
+            var context = new OwinContext(env);
+
+            try
+            {
+                var escherKey = escher.Authenticate(new OwinRequestAdapter(context.Request), keyDb);
+
+                logger(string.Format("EscherAuth successful for {0}", escherKey));
+
+                context.Request.User = new GenericPrincipal(new GenericIdentity(escherKey), new string[] { });
+            }
+            catch (EscherAuthenticationException e)
+            {
+                logger(string.Format("EscherAuth failed for {0} because {1}", context.Request.Uri, e.Message));
+            }
+
+            await next(env);
+        }
+
+    }
+}
---
+++ EscherOwinMiddleware/Request/OwinRequestAdapter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using EscherAuth.Request;
+using Microsoft.Owin;
+
+namespace EscherAuth.Middlewares.Owin.Request
+{
+
+    class OwinRequestAdapter : IEscherRequest
+    {
+        private readonly IOwinRequest request;
+        private string bodyCache = null;
+
+        public OwinRequestAdapter(IOwinRequest request)
+        {
+            this.request = request;
+            this.Headers = request.Headers.SelectMany(h => h.Value.Select(v => new Header(h.Key, v))).ToList();
+        }
+
+        public string Method => request.Method;
+        public Uri Uri => request.Uri;
+        public List<Header> Headers { get; }
+        public string Body
+        {
+            get
+            {
+                if (bodyCache != null)
+                {
+                    return bodyCache;
+                }
+
+                using (var reader = new StreamReader(request.Body))
+                {
+                    var bodyAsString = reader.ReadToEnd();
+                    bodyCache = bodyAsString;
+                    request.Body = new MemoryStream(Encoding.UTF8.GetBytes(bodyAsString));
+                }
+
+                return bodyCache;
+            }
+        }
+    }
+
+}
